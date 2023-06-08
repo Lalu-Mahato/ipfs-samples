348d3ad88eb079f2ac93313ce73c9d40a42ba35c
Lalu-Mahato <135944020+Lalu-Mahato@users.noreply.github.com>
2023-06-08 12:47:05 +0530

    Added env file

--- molaris-demo.js
+++ molaris-demo.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const Moralis = require('moralis');
 const fs = require('fs');
 
 (async () => {
     try {
         await Moralis.default.start({
-            apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjI0MzI1MTFlLWYzYzctNDhmNS1iZThlLWEwZDkxYTU3NzhlNyIsIm9yZ0lkIjoiMzQxOTg0IiwidXNlcklkIjoiMzUxNTYzIiwidHlwZUlkIjoiNzZjZGFkOWItZjJjMS00OGNkLTk5MmUtMWQxOTE4NWIxZjQyIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE2ODYxMzI2ODQsImV4cCI6NDg0MTg5MjY4NH0.GSz89YIOi7E9E2JOEQiata2bZrUZI673QK4YiMSa7J8"
+            apiKey: process.env.MOLARIS_API_KEY
         });
 
         const uploadArray = [
